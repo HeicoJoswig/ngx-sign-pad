81f74876491df1724215f20301fa1a40dec0ae7f
Heico Joswig <heico.joswig@gmail.com>
2019-03-31 01:15:37 +0100

    WIP: redraw on resize, works but needs to take ration into account

--- projects/sign-pad/src/lib/canvas-resize.directive.ts
+++ projects/sign-pad/src/lib/canvas-resize.directive.ts
@@ -11,29 +11,20 @@ import SignaturePad from 'signature_pad';
 @Directive({
   selector: '[libCanvasResize]'
 })
-export class CanvasResizeDirective implements OnInit, AfterViewInit {
+export class CanvasResizeDirective implements AfterViewInit {
 
   @Input() private signaturePad: SignaturePad;
 
-  // @ViewChild('canvasElement') canvasElementRef: ElementRef<HTMLCanvasElement>;
-  // @ViewChild() private containerElementRef: ElementRef<HTMLElement>;
-  // @ComponentRef() private canvasElementRef: ElementRef<HTMLCanvasElement>;
-
   constructor(private canvasElementRef: ElementRef<HTMLCanvasElement>, private renderer: Renderer2) {
-    console.log("ctor");
-  }
-
-  ngOnInit() {
-    console.log("onInit");
   }
 
   ngAfterViewInit() {
-    console.log('ngAfterViewInit');
     this.renderer.listen(window, 'resize', () => {
-      console.log("window resize");
+      const current = this.signaturePad.toDataURL();
       this.resizeCanvas();
+      this.signaturePad.clear();
+      this.signaturePad.fromDataURL(current);
     });
-    // window.addEventListener('resize', this.resizeCanvas);
     this.resizeCanvas();
   }
 
@@ -41,22 +32,13 @@ export class CanvasResizeDirective implements OnInit, AfterViewInit {
     console.log("canvas resize");
     const ratio = Math.max(window.devicePixelRatio || 1, 1);
     const canvas = this.canvasElementRef.nativeElement;
-    const container = this.canvasElementRef.nativeElement.parentElement;
 
-    console.log("ratio", ratio);
-    console.log("offsetWidth", canvas.offsetWidth);
-    console.log("offsetHeight", canvas.offsetHeight);
+    const container = this.renderer.parentNode(canvas); // this.canvasElementRef.nativeElement.parentElement;
 
-    // this.renderer.setStyle(canvas, 'width', canvas.offsetWidth * ratio);
-    // this.renderer.setStyle(canvas, 'height', canvas.offsetHeight * ratio);
-
-
-    this.renderer.setAttribute(canvas, 'width', Math.floor(container.offsetWidth * ratio).toString());
-    this.renderer.setAttribute(canvas, 'height', Math.floor(container.offsetHeight * ratio).toString());
-
+    this.renderer.setAttribute(canvas, 'width', (container.offsetWidth * ratio).toString());
+    this.renderer.setAttribute(canvas, 'height', (container.offsetHeight * ratio).toString());
 
     canvas.getContext('2d').scale(ratio, ratio);
-    this.signaturePad.clear(); // otherwise isEmpty() might return incorrect value
   }
 
 }
