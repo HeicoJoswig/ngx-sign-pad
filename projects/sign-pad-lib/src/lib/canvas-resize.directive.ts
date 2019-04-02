13753ea6d7c658765f340764eb685328b1cc98af
Heico Joswig <heico.joswig@gmail.com>
2019-04-02 21:28:14 +0200

    - added tunable debounce in resize callback
    - added retry in intial resize incase the parent element does not exist in ngAfterViewInit

--- projects/sign-pad-lib/src/lib/canvas-resize.directive.ts
+++ projects/sign-pad-lib/src/lib/canvas-resize.directive.ts
@@ -14,6 +14,7 @@ import { EImageType } from './models/image-type.enum';
 export class CanvasResizeDirective implements AfterViewInit {
 
   @Input() private signaturePad: SignaturePad;
+  @Input() private debounceTime?: number;
 
   constructor(private canvasElementRef: ElementRef<HTMLCanvasElement>, private renderer: Renderer2) {
   }
@@ -21,32 +22,38 @@ export class CanvasResizeDirective implements AfterViewInit {
   ngAfterViewInit() {
     this.renderer.listen(window, 'resize', () => {
       const currentSignature = this.signaturePad.toDataURL(EImageType.PNG);
-      this.resizeCanvas();
-      this.signaturePad.clear();
-      const canvas = this.canvasElementRef.nativeElement;
-      this.signaturePad.fromDataURL(currentSignature,
-        {
-          ratio: Math.max(window.devicePixelRatio || 1, 1),
-          height: parseFloat(canvas.getAttribute('height')),
-          width: parseFloat(canvas.getAttribute('width'))
-        },
-        (error) => {
-          if (error) {
-            this.signaturePad.clear();
-          }
-        });
+      setTimeout(() => {
+        this.resizeCanvas();
+        this.signaturePad.clear();
+        const canvas = this.canvasElementRef.nativeElement;
+        this.signaturePad.fromDataURL(currentSignature,
+          {
+            ratio: Math.max(window.devicePixelRatio || 1, 1),
+            height: parseFloat(canvas.getAttribute('height')),
+            width: parseFloat(canvas.getAttribute('width'))
+          },
+          (error) => {
+            if (error) {
+              this.signaturePad.clear();
+            }
+          });
+      }, this.debounceTime | 100);
     });
     this.resizeCanvas();
   }
 
-  resizeCanvas() {
+  private resizeCanvas(initial: boolean = false) {
     const canvas = this.canvasElementRef.nativeElement;
     const container = this.renderer.parentNode(canvas);
     const ratio = Math.max(window.devicePixelRatio || 1, 1);
 
-    this.renderer.setAttribute(canvas, 'width', (container.offsetWidth * ratio).toString());
-    this.renderer.setAttribute(canvas, 'height', (container.offsetHeight * ratio).toString());
-    canvas.getContext('2d').scale(ratio, ratio);
+    if (container.offsetHeight && container.offsetWidth) {
+      this.renderer.setAttribute(canvas, 'width', (container.offsetWidth * ratio).toString());
+      this.renderer.setAttribute(canvas, 'height', (container.offsetHeight * ratio).toString());
+      canvas.getContext('2d').scale(ratio, ratio);
+    } else if (initial) {
+      setTimeout(() => { this.resizeCanvas(true) }, this.debounceTime | 100);
+    }
   }
 
 }
